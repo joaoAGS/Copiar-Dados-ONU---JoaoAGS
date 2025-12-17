cb540861edc4f6d6e01776ceb7c044e7de9a1a29
João Augusto Marques Cardoso <joao.cardoso@ggnet.intra>
2025-12-17 08:52:49 -0300

    atualização

--- Copiar Dados ONU - JoaoAGS.user.js
+++ Copiar Dados ONU - JoaoAGS.user.js
@@ -12,8 +12,8 @@
 // @grant        GM_setClipboard
 // ==/UserScript==
 // --- ESTRATÉGIA DE ATUALIZAÇÃO ---
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://raw.githubusercontent.com/joaoAGS/Copiar-Dados-ONU---JoaoAGS/refs/heads/main/Copiar%20Dados%20ONU%20-%20JoaoAGS
+// @downloadURL  https://raw.githubusercontent.com/joaoAGS/Copiar-Dados-ONU---JoaoAGS/refs/heads/main/Copiar%20Dados%20ONU%20-%20JoaoAGS
 // 
 // ==/UserScript==
 
